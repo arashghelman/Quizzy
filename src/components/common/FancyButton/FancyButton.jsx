56d8575b59a0ad453bebdaa50ed03da417a00bb9
Arash Ghelman <arashghelman77@gmail.com>
2021-10-10 22:11:03 +0330

    Prettify files

--- src/components/common/FancyButton/FancyButton.jsx
+++ src/components/common/FancyButton/FancyButton.jsx
@@ -5,7 +5,8 @@ function FancyButton(props) {
     const {children, type, onButtonClicked} = props;
     
     return (
-      <button className={`${styles[`FancyButton`]} ${styles[`FancyButton_type-${type}`]}`} onClick={onButtonClicked}>
+      <button className={`${styles[`FancyButton`]} ${styles[`FancyButton_type-${type}`]}`} 
+              onClick={onButtonClicked}>
           {children}
       </button>  
     );
