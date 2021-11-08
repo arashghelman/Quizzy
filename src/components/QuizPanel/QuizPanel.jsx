e6d4dfb36535b8a2d3924485fd93af15bfffab8d
Arash Ghelman <arashghelman77@gmail.com>
2021-11-08 20:11:26 +0330

    Add jsx elements and style rules to QuizPanel component

--- src/components/QuizPanel/QuizPanel.jsx
+++ src/components/QuizPanel/QuizPanel.jsx
@@ -8,7 +8,26 @@ function QuizPanel() {
 
     return (
         <div className={styles['QuizPanel']}>
-            <OverviewForm />
+            <div className={styles['RightBar']}>
+                <div className={styles['OverviewSection']}>
+                    <div className={styles['OverviewSection__image']}>
+                        <i className="fas fa-image"></i>
+                        <span>Click here to upload a quiz image</span>
+                    </div>
+                    <div className={styles['OverviewSection__row']}>
+                        <h2>Untitled</h2>
+                        <button>
+                            <i className="fa fa-pencil" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                    <div className={styles['OverviewSection__row']}>
+                        <i className="fas fa-eye"></i>
+                        <span>Public</span>
+                        <i class="fa fa-sort" aria-hidden="true"></i>
+                        <span>Basic</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
