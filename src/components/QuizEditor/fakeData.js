1aab453c201fc10cd5180305ee6a2125ba7e2692
Arash Ghelman <arashghelman77@gmail.com>
2022-02-11 13:21:34 +0330

    Add fakeData.js to display external data in components

---
+++ src/components/QuizEditor/fakeData.js
@@ -0,0 +1,83 @@
+export const quizInfo = {
+  name: "Prepositions Test",
+  isPublic: true,
+  subjects: [
+    { id: "1", name: "English" },
+    { id: "2", name: "Fun" },
+    { id: "3", name: "World Languages" },
+  ],
+  grade: { min: "1st", max: "12th" },
+  description:
+    "A comprehensive quiz to test your general knowledge about English prepositions. I hope you enjoy it. ✨✨✨",
+  thumbnail: "https://blog.hubspot.com/hubfs/google-quiz.jpg",
+};
+
+export const questions = [
+  {
+    id: "1",
+    questionTitle: "See you ___ Friday.",
+    options: [
+      { id: "1", value: "in", isCorrect: false },
+      { id: "2", value: "on", isCorrect: true },
+      { id: "3", value: "at", isCorrect: false },
+    ],
+  },
+  {
+    id: "2",
+    questionTitle: "I brush my teeth ___ the morning.",
+    options: [
+      { id: "1", value: "at", isCorrect: false },
+      { id: "2", value: "in", isCorrect: true },
+      { id: "3", value: "on", isCorrect: false },
+    ],
+  },
+  {
+    id: "3",
+    questionTitle: "The match is ___ 4 o'clock.",
+    options: [
+      { id: "1", value: "at", isCorrect: true },
+      { id: "2", value: "on", isCorrect: false },
+      { id: "3", value: "in", isCorrect: false },
+    ],
+  },
+  {
+    id: "4",
+    questionTitle: "We have lunch ___ midday.",
+    options: [
+      { id: "1", value: "in", isCorrect: false },
+      { id: "2", value: "on", isCorrect: true },
+      { id: "3", value: "at", isCorrect: false },
+    ],
+  },
+  {
+    id: "5",
+    questionTitle: "My parents always visit me ___ my birthday.",
+    options: [
+      { id: "1", value: "at", isCorrect: false },
+      { id: "2", value: "on", isCorrect: true },
+      { id: "3", value: "in", isCorrect: false },
+    ],
+  },
+  {
+    id: "6",
+    questionTitle: "I'm busy ___ the moment.",
+    options: [
+      { id: "1", value: "at", isCorrect: true },
+      { id: "2", value: "on", isCorrect: false },
+      { id: "3", value: "in", isCorrect: false },
+    ],
+  },
+];
+
+export const quizSubjects = [
+  { id: "1", name: "Mathematics" },
+  { id: "2", name: "English" },
+  { id: "3", name: "Physics" },
+  { id: "4", name: "Chemistry" },
+  { id: "5", name: "Biology" },
+  { id: "6", name: "Computers" },
+  { id: "7", name: "World Languages" },
+  { id: "8", name: "Geography" },
+  { id: "9", name: "History" },
+  { id: "10", name: "Social Studies" },
+];
