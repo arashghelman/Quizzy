531ca3b0a4e57daa6495d96eed2ec0a848d03b33
Arash Ghelman <arashghelman77@gmail.com>
2022-02-20 19:01:06 +0330

    Add useSupabase.js custom hook file

---
+++ src/hooks/useSupabase.js
@@ -0,0 +1,33 @@
+import { useState, useEffect, useRef } from "react";
+
+export const useSupabase = (callback) => {
+  const callbackRef = useRef(callback);
+
+  const [fetchStatus, setFetchStatus] = useState({
+    data: null,
+    error: null,
+    isLoading: true,
+  });
+
+  useEffect(() => (callbackRef.current = callback), [callback]);
+
+  useEffect(() => {
+    callbackRef
+      .current()
+      .then((result) => {
+        setFetchStatus((current) => ({
+          ...current,
+          data: result.data,
+          error: null,
+        }));
+      })
+      .catch((error) => {
+        setFetchStatus((current) => ({ ...current, data: null, error: error }));
+      })
+      .finally(() =>
+        setFetchStatus((current) => ({ ...current, isLoading: false }))
+      );
+  }, []);
+
+  return fetchStatus;
+};
