756932e624841810dfbfe6308a8a1c1f6d9968e6
Arash Ghelman <arashghelman77@gmail.com>
2022-02-24 09:59:30 +0330

    Refactor component files
    - onClick callback is passed directly to onClick event handler of Button.jsx.
    - Removed onSubmit callback from Form.jsx.

--- src/components/shared/Form.jsx
+++ src/components/shared/Form.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Button from "./Button";
 
-export default function Form({ children, buttonText = "Save", onSubmit }) {
+export default function Form({ children, buttonText = "Save" }) {
   return (
     <form className="flex flex-col gap-6">
       {children}
       <div className="grid grid-flow-col auto-cols-fr justify-end gap-x-3 mb-2">
         <div className="col-span-3"></div>
-        <Button type="submit" variant="greenish" onClick={onSubmit}>
+        <Button type="submit" variant="greenish">
           {buttonText}
         </Button>
       </div>
