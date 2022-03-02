a0410bc509dc74116e4e910133c53498fc0ce5e5
Arash Ghelman <arashghelman77@gmail.com>
2022-03-02 20:11:21 +0330

    Refactor App.js
    A supabase client is created in App.js and passed to Provider's value parameter.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import "./App.css";
+import { createClient } from "@supabase/supabase-js";
+import { Provider } from "react-supabase";
 import QuizEditor from "./components/QuizEditor/QuizEditor";
 import UserProfile from "./components/UserProfile/UserProfile";
 import QuizPlayer from "./components/QuizPlayer/QuizPlayer";
 
+const client = createClient(
+  "https://magomretapvzwepvnkiv.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hZ29tcmV0YXB2endlcHZua2l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ2NjgwMzgsImV4cCI6MTk2MDI0NDAzOH0.f2Cmnv4Fxwsp1DdV-zujupT9cGgj9Q-Cg0AMlwkUWGk"
+);
+
 function App() {
   return (
-    <div className="App">
-      <div className="mx-32 h-full">
-        <QuizEditor />
-        {/* <UserProfile /> */}
+    <Provider value={client}>
+      <div className="App">
+        <div className="mx-32 h-full">
+          <QuizEditor />
+          {/* <UserProfile /> */}
+        </div>
+        {/* <QuizPlayer /> */}
       </div>
-      {/* <QuizPlayer /> */}
-    </div>
+    </Provider>
   );
 }
 
